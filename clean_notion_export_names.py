b6a0d8c2b5d130991e8b970bd073c2dc7fc8f0e2
ZhouyaoXie <zhouyaox@andrew.cmu.edu>
2023-11-21 15:41:13 -0800

    remove _all extension & save page link map

--- clean_notion_export_names.py
+++ clean_notion_export_names.py
@@ -1,28 +1,45 @@
 import os
 import re
 import shutil
+import json 
 
 def clean_name(name):
     # Regular expression to find the random string pattern followed by a file extension or at the end
-    pattern = r' \w{32}(?=\.\w+|$)'
+    pattern = re.compile(' \w{32}(?=\.\w+|$|(?=_all))')
+    match = pattern.search(name)
     # Remove the pattern if found and return the cleaned name
-    return re.sub(pattern, '', name)
+    if match:
+        cleaned_name = re.sub(pattern, '', name).strip('')
+        matched_str = name[match.span()[0]:match.span()[1]].strip('_all')
+        return cleaned_name, matched_str
+    else: 
+        print(f"no match found for {name}")
+        return name, None 
 
-def _rename(root):
+def _rename(root, d):
     for file in os.listdir(root):
-        clean_file_name = clean_name(file)
+        clean_file_name, matched_str = clean_name(file)
         if clean_file_name != file:
             original_file_path = os.path.join(root, file)
             new_file_path = os.path.join(root, clean_file_name)
             print(f'Renaming file: {original_file_path} to {new_file_path}')
+            d[clean_file_name] = matched_str
             shutil.move(original_file_path, new_file_path)
 
-def rename(root):
-    _rename(root)
+def rename(root, d):
+    _rename(root, d)
     for item in os.listdir(root):
         item_path = os.path.join(root, item)
 
         if os.path.isdir(item_path):
-            rename(item_path)
+            rename(item_path, d)
 
-rename("data")
+if __name__ == '__main__':
+    d = {}
+    data_dir = "data"
+
+    rename(data_dir, d)
+
+    # save directory name file to data directory 
+    with open(os.path.join(data_dir, "directory_name.json"), 'w') as json_file:
+        json.dump(d, json_file)
